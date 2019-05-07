66a1f885ccc72267c52bd8b7a42e23549554506f
ketanchoyal <sharmaketanr@gmail.com>
2019-05-07 21:43:41 +0530

    Optimization

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export default {
     // npx kill-port 3006
-    "port": process.env.PORT || 3000,
+    "port": process.env.PORT,
     "mongoUrl": "mongodb://choyalketan:PM5SkuCuvEkaeUr@ds056789.mlab.com:56789/foodtruck-api",
     // "port": 3006,
     // "mongoUrl": "mongodb://localhost:27017/foodtruck-api",
